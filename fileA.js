6c9c709a7d868c1e7061e8373adfc02de551c091
HannahWroblewski <hannah.wrob@gmail.com>
2023-10-14 15:07:37 -0400

    2023 update

---
+++ fileA.js
@@ -0,0 +1,19 @@
+// const exportsFromFileB = require('./fileB') // note the relative file path
+// console.log(exportsFromFileB)
+// console.log(exportsFromFileB.foo)
+// console.log(exportsFromFileB.bar)
+
+// const exportsFromFileB = require('./fileB')
+// exportsFromFileB()
+
+// require('./fileB')
+// require('./fileB') // not a typo - actually require it twice!
+
+// const exportsFromSnippet1 =require('./snippet1')
+// console.log(exportsFromSnippet1)
+
+// const exportsFromSnippet2 = require('./snippet2')
+// console.log(exportsFromSnippet2)
+
+const exportsFromSnippet3 = require('./snippet3')
+console.log(exportsFromSnippet3)
